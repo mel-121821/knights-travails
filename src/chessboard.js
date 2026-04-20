6981a38a918702658c9283a828b7c827d785d5ec
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-04-20 15:26:12 -0400

    Move Chessboard class to its own module and export to script.js

---
+++ src/chessboard.js
@@ -0,0 +1,44 @@
+const Chessboard = (function () {
+  const rows = 8;
+  const columns = 8;
+  const marker = "0";
+  const recent = "X";
+  const potential = "?";
+  const grid = makeGrid();
+
+  function makeGrid() {
+    const grid = [];
+    for (let i = 0; i < rows; i++) {
+      grid[i] = [];
+      for (let j = 0; j < columns; j++) {
+        grid[i].push("");
+      }
+    }
+    return grid;
+  }
+
+  function markCurrentPlace(x, y) {
+    grid[x][y] = marker;
+  }
+
+  function markRecentPlace(x, y) {
+    grid[x][y] = recent;
+  }
+
+  function markPotentials(coordsList) {
+    for (let coords of coordsList) {
+      grid[coords[0]][coords[1]] = potential;
+    }
+  }
+
+  function clearGrid() {
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < columns; j++) {
+        grid[j] = "";
+      }
+    }
+  }
+  return { grid, markCurrentPlace, markRecentPlace, markPotentials, clearGrid };
+})();
+
+export { Chessboard };
