6ea71d855f20e7431be857c365c4ee49236de4ea
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-04-21 15:31:16 -0400

    Change tests evaluating potential move arrs from .toContainEqual() to .toMatchObject() to better match the data structure and be more concise

--- src/test.js
+++ src/test.js
@@ -8,18 +8,36 @@ test("Knight class exists", () => {
 });
 
 test("knight at [7, 1] has the following potential coords: [5, 2], [5, 0], [6, 3]", () => {
-  expect(knight.getAvailableMoves(7, 1)).toContainEqual([5, 2]);
-  expect(knight.getAvailableMoves(7, 1)).toContainEqual([5, 0]);
-  expect(knight.getAvailableMoves(7, 1)).toContainEqual([6, 3]);
+  expect(knight.getAvailableMoves(7, 1)).toMatchObject([
+    [5, 2],
+    [5, 0],
+    [6, 3],
+  ]);
 });
 
 test("knight at [3, 3] has the following potential coords: [1, 4], [1, 2], [5, 4], [5, 2], [2, 5], [2, 1], [4, 5], [4, 1]", () => {
-  expect(knight.getAvailableMoves(3, 3)).toContainEqual([1, 4]);
-  expect(knight.getAvailableMoves(3, 3)).toContainEqual([1, 2]);
-  expect(knight.getAvailableMoves(3, 3)).toContainEqual([5, 4]);
-  expect(knight.getAvailableMoves(3, 3)).toContainEqual([5, 2]);
-  expect(knight.getAvailableMoves(3, 3)).toContainEqual([2, 5]);
-  expect(knight.getAvailableMoves(3, 3)).toContainEqual([2, 1]);
-  expect(knight.getAvailableMoves(3, 3)).toContainEqual([4, 5]);
-  expect(knight.getAvailableMoves(3, 3)).toContainEqual([4, 1]);
+  expect(knight.getAvailableMoves(3, 3)).toMatchObject([
+    [1, 4],
+    [1, 2],
+    [5, 4],
+    [5, 2],
+    [2, 5],
+    [2, 1],
+    [4, 5],
+    [4, 1],
+  ]);
 });
+
+test("knightMoves exists", () => {
+  expect(knight.knightMoves).toBeDefined();
+});
+
+test("knightMoves returns q", () => {
+  expect(knight.knightMoves(7, 1)).toMatchObject([
+    [5, 2],
+    [5, 0],
+    [6, 3],
+  ]);
+});
+
+// test("knightMoves");
