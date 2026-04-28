21124a106089e2175c814c27648c11e8fc120a2c
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-04-28 14:02:17 -0400

    Create 3 tests for knightMoves() with different end coords and path expectations

--- src/test.js
+++ src/test.js
@@ -32,12 +32,16 @@ test("knightMoves exists", () => {
   expect(knight.knightMoves).toBeDefined();
 });
 
-test("knightMoves returns q", () => {
-  expect(knight.knightMoves(7, 1)).toMatchObject([
-    [5, 2],
-    [5, 0],
-    [6, 3],
-  ]);
+test("knightMoves(3, 3) with starting point [7, 1] returns 2 moves", () => {
+  expect(knight.knightMoves(3, 3)).toBe(2);
+});
+
+test("knightMoves(1, 4) with starting point [7, 1] returns 3 moves", () => {
+  expect(knight.knightMoves(1, 4)).toBe(3);
+});
+
+test("knightMoves(0, 2) with starting point [7, 1] returns 4 moves", () => {
+  expect(knight.knightMoves(0, 2)).toBe(4);
 });
 
 // test("knightMoves");
