85c4d3e3c3827324c8e6f3be4d32c102dbdd812a
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-04-28 14:09:08 -0400

    Add adjacencyGraph class to create a graph, add adjacency arrays, mark coords as visited and return if coord set isVisited

--- src/node.js
+++ src/node.js
@@ -1,14 +1,46 @@
 class Node {
-  constructor(prevNode = null, x, y) {
+  constructor(x, y, prevNode = null) {
     this.x = x;
     this.y = y;
     this.visited = false;
     this.prevNode = prevNode;
+    this.nextNodes = [];
   }
 
-  static getNode() {
+  static getNode(x, y) {
     return Node;
   }
 }
 
-export { Node };
+class AdjacencyGraph {
+  constructor() {
+    this.rows = 8;
+    this.columns = 8;
+    this.graph = this.makeGraph();
+  }
+
+  makeGraph() {
+    const graph = [];
+    for (let i = 0; i < this.rows; i++) {
+      graph[i] = [];
+      for (let j = 0; j < this.columns; j++) {
+        graph[i].push("");
+      }
+    }
+    return graph;
+  }
+
+  addAdj(x, y, arr) {
+    this.graph[x][y] = arr;
+  }
+
+  visited(x, y) {
+    this.graph[x][y] = true;
+  }
+
+  isVisited(x, y) {
+    return this.graph[x][y];
+  }
+}
+
+export { Node, AdjacencyGraph };
