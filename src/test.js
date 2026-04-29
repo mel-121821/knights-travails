2c95b3a60dc20ec2d6a1b4042efb37d6de67698b
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-04-29 14:48:23 -0400

    Update tests to reflect param change from x, y variables to coords arr

--- src/test.js
+++ src/test.js
@@ -1,14 +1,13 @@
 //imports
-import { Knight } from "./script";
 import { knight } from "./script";
 
 // tests
-test("Knight class exists", () => {
-  expect(Knight).toBeDefined();
+test("knight instance exists", () => {
+  expect(knight).toBeDefined();
 });
 
 test("knight at [7, 1] has the following potential coords: [5, 2], [5, 0], [6, 3]", () => {
-  expect(knight.getAvailableMoves(7, 1)).toMatchObject([
+  expect(knight.getAvailableMoves([7, 1])).toMatchObject([
     [5, 2],
     [5, 0],
     [6, 3],
@@ -16,7 +15,7 @@ test("knight at [7, 1] has the following potential coords: [5, 2], [5, 0], [6, 3
 });
 
 test("knight at [3, 3] has the following potential coords: [1, 4], [1, 2], [5, 4], [5, 2], [2, 5], [2, 1], [4, 5], [4, 1]", () => {
-  expect(knight.getAvailableMoves(3, 3)).toMatchObject([
+  expect(knight.getAvailableMoves([3, 3])).toMatchObject([
     [1, 4],
     [1, 2],
     [5, 4],
@@ -33,15 +32,15 @@ test("knightMoves exists", () => {
 });
 
 test("knightMoves(3, 3) with starting point [7, 1] returns 2 moves", () => {
-  expect(knight.knightMoves(3, 3)).toBe(2);
+  expect(knight.knightMoves([7, 1], [3, 3])).toBe(2);
 });
 
 test("knightMoves(1, 4) with starting point [7, 1] returns 3 moves", () => {
-  expect(knight.knightMoves(1, 4)).toBe(3);
+  expect(knight.knightMoves([7, 1], [1, 4])).toBe(3);
 });
 
 test("knightMoves(0, 2) with starting point [7, 1] returns 4 moves", () => {
-  expect(knight.knightMoves(0, 2)).toBe(4);
+  expect(knight.knightMoves([7, 1], [0, 2])).toBe(4);
 });
 
 // test("knightMoves");
