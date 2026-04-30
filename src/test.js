adf70e2cffd15fb9a26f479301c9e287bd930737
Megan Lindsay <97409605+mel-121821@users.noreply.github.com>
2026-04-30 14:24:25 -0400

    Change knightMoves() output to a path instead of moveCount. Comment out affected tests and create new ones expecting a path

--- src/test.js
+++ src/test.js
@@ -31,16 +31,33 @@ test("knightMoves exists", () => {
   expect(knight.knightMoves).toBeDefined();
 });
 
-test("knightMoves(3, 3) with starting point [7, 1] returns 2 moves", () => {
-  expect(knight.knightMoves([7, 1], [3, 3])).toBe(2);
+// Testing moveCount
+// test("knightMoves(3, 3) with starting point [7, 1] returns 2 moves", () => {
+//   expect(knight.knightMoves([7, 1], [3, 3])).toBe(2);
+// });
+
+// test("knightMoves(1, 4) with starting point [7, 1] returns 3 moves", () => {
+//   expect(knight.knightMoves([7, 1], [1, 4])).toBe(3);
+// });
+
+// test("knightMoves(0, 2) with starting point [7, 1] returns 4 moves", () => {
+//   expect(knight.knightMoves([7, 1], [0, 2])).toBe(4);
+// });
+
+// Testing path
+test("knightMoves(3, 3) with starting point [7, 1] returns path [[7, 1], [5, 2], [3, 3]]", () => {
+  expect(knight.knightMoves([7, 1], [3, 3])).toMatchObject([
+    [7, 1],
+    [5, 2],
+    [3, 3],
+  ]);
 });
 
-test("knightMoves(1, 4) with starting point [7, 1] returns 3 moves", () => {
-  expect(knight.knightMoves([7, 1], [1, 4])).toBe(3);
+test("knightMoves(4, 3) with starting point [3, 3] returns path [[3, 3], [1, 4], [3, 5], [4, 3]]", () => {
+  expect(knight.knightMoves([3, 3], [4, 3])).toMatchObject([
+    [3, 3],
+    [1, 4],
+    [3, 5],
+    [4, 3],
+  ]);
 });
-
-test("knightMoves(0, 2) with starting point [7, 1] returns 4 moves", () => {
-  expect(knight.knightMoves([7, 1], [0, 2])).toBe(4);
-});
-
-// test("knightMoves");
